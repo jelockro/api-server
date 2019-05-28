e73144d58d2cce0d95dff9e2b6f2b17bb0824cd8
jelockro <jlocckrow@gmail.com>
2019-05-28 17:22:29 +0000

    updated requirements.txt

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/vagrant/PycharmProjects/zeus-api-applicationl/venv"
+set -gx VIRTUAL_ENV "/home/vagrant/PycharmProjects/zeus-api-application/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
